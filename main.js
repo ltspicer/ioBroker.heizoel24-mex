c3e655cdb37acd3be8d2b3c03c11cf3c64b4d4df
ltspicer <dlu0@sunrise.ch>
2024-03-02 20:04:43 +0100

    schedule und eigenen code eingefügt

--- main.js
+++ main.js
@@ -9,7 +9,9 @@
 const utils = require("@iobroker/adapter-core");
 
 // Load your modules here, e.g.:
-// const fs = require("fs");
+
+const axios = require('axios');
+const mqtt = require('mqtt');
 
 class Heizoel24Mex extends utils.Adapter {
 
@@ -26,6 +28,15 @@ class Heizoel24Mex extends utils.Adapter {
         // this.on("objectChange", this.onObjectChange.bind(this));
         // this.on("message", this.onMessage.bind(this));
         this.on("unload", this.onUnload.bind(this));
+        const session_id = "";
+        const debug = false;
+        const username = this.config.username;
+        const passwort = this.config.passwort;
+        const broker_address = this.config.broker_address;
+        const mqtt_active = this.config.mqtt_active;
+        const mqtt_user = this.config.mqtt_user;
+        const mqtt_pass = this.config.mqtt_pass;
+        this.log.info("constructor ausgeführt!");
     }
 
     /**
@@ -39,25 +50,172 @@ class Heizoel24Mex extends utils.Adapter {
 
         // The adapters config (in the instance object everything under the attribute "native") is accessible via
         // this.config:
-        this.log.info("config option1: " + this.config.option1);
-        this.log.info("config option2: " + this.config.option2);
+        this.log.info("config username: " + username);
+        this.log.info("config passwort: " + this.config.passwort);
+        this.log.info("config broker_address: " + this.config.broker_address);
+        this.log.info("config mqtt_active: " + this.config.mqtt_active);
+        this.log.info("config mqtt_user: " + this.config.mqtt_user);
+        this.log.info("config mqtt_pass: " + this.config.mqtt_pass);
 
-        /*
-        For every state in the system there has to be also an object of type state
-        Here a simple template for a boolean variable named "testVariable"
-        Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-        */
-        await this.setObjectNotExistsAsync("testVariable", {
-            type: "state",
-            common: {
-                name: "testVariable",
-                type: "boolean",
-                role: "indicator",
-                read: true,
-                write: true,
-            },
-            native: {},
+    async function mqtt_send(client, topic, wert) {
+        client.publish("MEX/" + topic, wert);
+    }
+    
+    async function login() {
+        if (debug) {
+            console.log('Login in...');
+        }
+        const url = "https://api.heizoel24.de/app/api/app/Login";
+        const newHeaders = { 'Content-type': 'application/json' };
+        try {
+            const reply = await axios.post(url, { "Password": passwort, "Username": username }, { headers: newHeaders });
+            if (reply.status === 200) {
+                if (debug) {
+                    console.log("Login OK");
+                }
+                const reply_json = reply.data;
+                if (reply_json['ResultCode'] === 0) {
+                    session_id = reply_json['SessionId'];
+                    if (debug) {
+                        console.log('Session ID: ' + session_id);
+                    }
+                    return true;
+                } else {
+                    if (debug) {
+                        console.log('ResultCode nicht 0. Keine Session ID erhalten!');
+                    }
+                }
+            }
+        } catch (error) {
+            if (debug) {
+                console.log('Login fehlgeschlagen! Heizoel24 Login Status Code: ' + error.response.status);
+            }
+        }
+        return false;
+    }
+    
+    async function mex() {
+        const login_status = await login();
+        if (!login_status) {
+            return "error";
+        }
+        if (debug) {
+            console.log('Refresh sensor data cache...');
+        }
+        const url = `https://api.heizoel24.de/app/api/app/GetDashboardData/${session_id}/1/1/False`;
+        try {
+            const reply = await axios.get(url);
+            if (reply.status === 200) {
+                if (debug) {
+                    console.log("Daten wurden empfangen");
+                }
+                return reply;
+            }
+        } catch (error) {
+            if (debug) {
+                console.log('Heizoel24 GetDashboardData Status Code: ' + error.response.status);
+            }
+        }
+        return "error";
+    }
+    
+    async function main() {
+        const topic1 = ['SensorId', 'IsMain', 'CurrentVolumePercentage', 'CurrentVolume', 'NotifyAtLowLevel', 'NotifyAtAlmostEmptyLevel', 'NotificationsEnabled', 'Usage', 'RemainsUntil', 'MaxVolume', 'ZipCode', 'MexName', 'LastMeasurementTimeStamp', 'LastMeasurementWithDifferentValue', 'BatteryPercentage', 'Battery', 'LitresPerCentimeter', 'LastMeasurementWasSuccessfully', 'SensorTypeId', 'HasMeasurements', 'MeasuredDaysCount', 'LastMeasurementWasTooHigh', 'YearlyOilUsage', 'RemainingDays', 'LastOrderPrice', 'ResultCode', 'ResultMessage'];
+        const topic2 = ['LastOrderPrice', 'PriceComparedToYesterdayPercentage', 'PriceForecastPercentage', 'HasMultipleMexDevices', 'DashboardViewMode', 'ShowComparedToYesterday', 'ShowForecast', 'ResultCode', 'ResultMessage'];
+        const RemainsUntilCombined = ['MonthAndYear', 'RemainsValue', 'RemainsUnit'];
+    
+        const client = mqtt.connect(`mqtt://${broker_address}`, {
+            username: mqtt_user,
+            password: mqtt_pass
         });
+    
+        const daten = await mex();
+        if (daten === "error") {
+            if (debug) {
+                console.log("Fehler. Keine Daten empfangen.");
+            }
+            await mqtt_send(client, "Items/DataReceived", false);
+            client.end();
+            return;
+        }
+        const datenJson = daten.data;
+        if (debug) {
+            console.log();
+            console.log("JSON-Daten:");
+            console.log("===========");
+            console.log();
+            console.log(datenJson);
+            console.log();
+            console.log("---------------------");
+            console.log();
+        }
+        for (let n = 0; n < topic2.length; n++) {
+            if (debug) {
+                console.log(topic2[n] + ":", datenJson[topic2[n]]);
+            }
+            const result = datenJson[topic2[n]] || 'leer';
+            await mqtt_send(client, "PricingForecast/" + topic2[n], result.toString());
+        }
+        let items = datenJson["Items"][0];
+        if (debug) {
+            console.log("---------------------");
+        }
+        for (let n = 0; n < topic1.length; n++) {
+            if (debug) {
+                console.log(topic1[n] + ":", items[topic1[n]]);
+            }
+            const result = items[topic1[n]] || 'leer';
+            await mqtt_send(client, "Items/" + topic1[n], result.toString());
+        }
+        await mqtt_send(client, "Items/DataReceived", 'true');
+        let daten3 = items['RemainsUntilCombined'];
+        if (debug) {
+            console.log("---------------------");
+            console.log('RemainsUntilCombined:');
+        }
+        for (let n = 0; n < RemainsUntilCombined.length; n++) {
+            if (debug) {
+                console.log(RemainsUntilCombined[n] + ":", daten3[RemainsUntilCombined[n]]);
+            }
+            const result = daten3[RemainsUntilCombined[n]] || 'leer';
+            await mqtt_send(client, "RemainsUntilCombined/" + RemainsUntilCombined[n], result.toString());
+        }
+        client.end();
+
+        this.subscribeStates("username");
+        await this.setStateAsync("username", username);
+    
+    }
+    
+    await this.setObjectNotExistsAsync("username", {
+        type: "state",
+        common: {
+            name: "username",
+            type: "string",
+            role: "name",
+            read: true,
+            write: true,
+        },
+        native: {},
+    });
+
+
+    /*
+    For every state in the system there has to be also an object of type state
+    Here a simple template for a boolean variable named "testVariable"
+    Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
+    */
+    await this.setObjectNotExistsAsync("testVariable", {
+        type: "state",
+        common: {
+            name: "testVariable",
+            type: "boolean",
+            role: "indicator",
+            read: true,
+            write: true,
+        },
+        native: {},
+    });
 
         // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
         this.subscribeStates("testVariable");
