cdec6d0391739df13a5d1d9db71d6cd6c60d9233
Daniel <dlu0@sunrise.ch>
2024-12-02 11:49:52 +0100

    Update eslint.config.mjs
    Div corrections

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,45 +9,39 @@ const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+    allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: ["**/.eslintrc.js", "admin/words.js"],
-}, ...compat.extends("eslint:recommended"), {
-    plugins: {},
-
-    languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.mocha,
-        },
-
-        ecmaVersion: "latest",
-        sourceType: "module",
-    }
-
-    rules: {
-        indent: ["error", 4, {
-            SwitchCase: 1,
-        }],
-
-        "no-console": "off",
-
-        "no-unused-vars": ["error", {
-            ignoreRestSiblings: true,
-            argsIgnorePattern: "^_",
-        }],
-
-        "no-var": "error",
-        "no-trailing-spaces": "error",
-        "prefer-const": "error",
-
-        quotes: ["error", "double", {
-            avoidEscape: true,
-            allowTemplateLiterals: true,
-        }],
-
-        semi: ["error", "always"],
+export default [
+    {
+        ignores: ["**/.eslintrc.js", "admin/words.js"],
     },
-}];
+    ...compat.extends("eslint:recommended"),
+    {
+        plugins: {},
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.mocha,
+            },
+            ecmaVersion: "latest",
+            sourceType: "module",
+        },
+        rules: {
+            indent: ["error", 4, { SwitchCase: 1 }],
+            "no-console": "off",
+            "no-unused-vars": [
+                "error",
+                {
+                    ignoreRestSiblings: true,
+                    argsIgnorePattern: "^_",
+                },
+            ],
+            "no-var": "error",
+            "no-trailing-spaces": "error",
+            "prefer-const": "error",
+            quotes: ["error", "double", { avoidEscape: true, allowTemplateLiterals: true }],
+            semi: ["error", "always"],
+        },
+    },
+];
